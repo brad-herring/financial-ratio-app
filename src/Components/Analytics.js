b75d4124121d358b67e91beae7a0302c6b820ea6
Herschel Herring <hbradherring@aol.com>
2019-04-13 16:31:47 -0400

    Working APIs

--- src/Components/Analytics.js
+++ src/Components/Analytics.js
@@ -14,13 +14,24 @@ class Analytics extends Component {
   getData = async (e) => {
     e.preventDefault();
     const ticker = this.state.ticker;
+
     const income_call = await fetch(`https://financialmodelingprep.com/api/financials/income-statement/${ticker}?period=quarter&datatype=json`);
+    const income_data = await income_call.json();
+
     const balance_call = await fetch(`https://cors-anywhere.herokuapp.com/https://financialmodelingprep.com/api/financials/balance-sheet-statement/${ticker}?period=quarter&datatype=json`);
+    const balance_data = await balance_call.json();
+
     const cash_call = await fetch(`https://cors-anywhere.herokuapp.com/https://financialmodelingprep.com/api/financials/cash-flow-statement/${ticker}?period=quarter&datatype=json`);
+    const cash_data = await cash_call.json();
+
     const rating_call = await fetch(`https://cors-anywhere.herokuapp.com/https://financialmodelingprep.com/api/company/rating/${ticker}?datatype=json`);
+    const rating_data = await rating_call.json();
 
     console.log(ticker);
-    console.log(income_call);
+    console.log(income_data);
+    console.log(balance_data);
+    console.log(cash_data);
+    console.log(rating_data);
   }
 
   render() {
