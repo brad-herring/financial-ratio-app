0ec046d3d17bdafdbcc6109e847e922fc19125a2
Herschel Herring <hbradherring@aol.com>
2019-04-17 15:34:32 -0400

    Updates

---
+++ precache-manifest.eda7c3a8ed09d48be9e04b3253812280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af33de56f690439eb0bf",
+    "url": "/financial-ratio-app/static/js/runtime~main.def9ee0a.js"
+  },
+  {
+    "revision": "88156692d84b57cea6da",
+    "url": "/financial-ratio-app/static/js/main.013a306a.chunk.js"
+  },
+  {
+    "revision": "a439aa0e02590e85eff4",
+    "url": "/financial-ratio-app/static/js/2.513a94c9.chunk.js"
+  },
+  {
+    "revision": "88156692d84b57cea6da",
+    "url": "/financial-ratio-app/static/css/main.cd4f7e2e.chunk.css"
+  },
+  {
+    "revision": "a439aa0e02590e85eff4",
+    "url": "/financial-ratio-app/static/css/2.b70b2c05.chunk.css"
+  },
+  {
+    "revision": "a5baaaf65ba9b917a53c27cfd25a1f87",
+    "url": "/financial-ratio-app/index.html"
+  }
+];
